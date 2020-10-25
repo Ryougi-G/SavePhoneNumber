389496f2efa830aba93b2552878e07e5392c5561
Ryougi-G <1365654168@qq.com>
2020-10-25 11:42:40 +0800

    Create

---
+++ SavePhoneNumber/Config.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SavePhoneNumber
+{
+    class Config
+    {
+        public int accountNumber;
+        public List<Account> accounts;
+        public bool useProxy;
+        public string proxyIP;
+        public string proxyPort;
+    }
+    class Account
+    {
+        public int username;
+        public int password;
+        public int taskNumber;
+        public List<string> taskFiles;
+    }
+    class TNTask
+    {
+        string targetNumber;
+    }
+}
