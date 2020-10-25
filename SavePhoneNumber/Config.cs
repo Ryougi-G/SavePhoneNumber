b0ff3a0bba7a54f20bfca973d683e22fbabc51eb
Ryougi-G <1365654168@qq.com>
2020-10-25 12:08:09 +0800

    编写配置系统

--- SavePhoneNumber/Config.cs
+++ SavePhoneNumber/Config.cs
@@ -3,26 +3,70 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.IO;
 
 namespace SavePhoneNumber
 {
     class Config
     {
-        public int accountNumber;
+        public int accountNumber {
+            get
+            {
+                return accounts.Count;
+            }
+        }
         public List<Account> accounts;
+        public int timeToExcuteTasks;
         public bool useProxy;
-        public string proxyIP;
-        public string proxyPort;
+        public WebProxy Proxy;
+        public Config(List<Account> acts,int timeToExcuteTasks)
+        {
+            accounts = acts;
+            this.timeToExcuteTasks = timeToExcuteTasks;
+            useProxy = false;
+        }
+        public Config(List<Account> acts, int timeToExcuteTasks,WebProxy proxy)
+        {
+            accounts = acts;
+            this.timeToExcuteTasks = timeToExcuteTasks;
+            useProxy = true ;
+            proxy = Proxy;
+        }
     }
     class Account
     {
-        public int username;
-        public int password;
+        public string username;
+        public string password;
         public int taskNumber;
-        public List<string> taskFiles;
+        public List<TNTask> Tasks;
+        public Account(string UserName,string Password,List<TNTask> tasks)
+        {
+            username = UserName;
+            password = Password;
+            Tasks = tasks;
+        }
     }
     class TNTask
     {
-        string targetNumber;
+        public string targetNumber;
+        public List<string> messages;
+        public TNTask(string targetNum,List<string> msg)
+        {
+            targetNumber = targetNum;
+            messages = msg;
+        }
+    }
+    public class ConfigReader
+    {
+        static Config ReadConfig(string FilePos) 
+        {
+            if (!File.Exists(FilePos))
+            {
+                throw new FileNotFoundException("Config File Doesn't exist");
+            }
+            string[] FileStr = File.ReadAllLines(FilePos);
+
+        }
     }
 }
